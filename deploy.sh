176f34d3321675a2c69059ad011e3f298e03f9e4
AkshataSJ <sudhindr.j@gmail.com>
2018-10-04 16:57:31 +0530

    username ip address changed in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
-
+#
 
 if [ $ENVIRONMENT = "QA" ];then
-sshpass -p "gamutp" scp target/tacobell.war gamutp@172.17.0.3:/home/gamutp/distros/apache-tomcat/webapps
+sshpass -p "ccd" scp target/tacobell.war ccd@172.17.0.5:/home/ccd/distros/apache-tomcat/webapps
          echo "Starting tomcat server in $i QA server"
          sleep 3
-sshpass -p "gamutp" ssh gamutp@172.17.0.3 "JAVA_HOME=/home/gamutp/jdk1.8.0_181"/home/gamutp/distros/apache-tomcat/bin/startup.sh
+sshpass -p "ccd" ssh ccd@172.17.0.5 "JAVA_HOME=/home/gamutp/jdk1.8.0_181"/home/ccd/distros/apache-tomcat/bin/startup.sh
 
 elif [$ENVIRONMENT= "SIT"] 
 
 then
 
 
-sshpass -p "sitdeploy" scp target/tacobell.war sitdeploy@172.17.0.2:/home/sitdeploy/distros/apache-tomcat/webapps
+sshpass -p "dev2team" scp target/tacobell.war dev2team@172.17.0.4:/home/dev2team/distros/apache-tomcat/webapps
          echo "Starting tomcat server in $i QA server"
          sleep 3
-sshpass -p "sitdeploy" ssh sitdeploy@172.17.0.2 "JAVA_HOME=/home/sitdeploy/distros/jdk1.8"/home/sitdeploy/distros/apache-tomcat/bin/startup.sh
+sshpass -p "dev2team" ssh dev2team@172.17.0.4 "JAVA_HOME=/home/dev2team/distros/jdk1.8"/home/dev2team/distros/apache-tomcat/bin/startup.sh
 
 
 
